efbea0b514feb50cd4f07387cf533f1736c337e1
MikeyHack3712 <mikeelie.me@gmail.com>
2016-12-03 16:12:30 -0600

    unit test update

--- evacuspots_test.py
+++ evacuspots_test.py
@@ -3,25 +3,33 @@ import unittest
 
 class TestEvacuspotJson(unittest.TestCase):
     required_fields = [
-        'address', 'latitude', 'longitude', 'name', 'neighborhood', 'special_needs_location', 'spot_number', 'zip'
+        'address', 'name', 'neighborhood', 'special_needs_location', 'spot_number', 'zip'
     ]
 
     def setUp(self):
-        f = open("evacuspots.json")
-        self.evacuspots = json.load(f)
+        f = open("evacuspots.geojson")
+        self.evacuspots = json.load(f)['features']
         f.close()
 
     def test_required_fields(self):
         for evacuspot in self.evacuspots:
             for field in self.required_fields:
-                self.assertTrue(field in evacuspot, 'Missing field {}'.format(field))
+                self.assertTrue(field in evacuspot['properties'], 'Missing field {}'.format(field))
 
+    def test_geometry(self):        
+        for evacuspot in self.evacuspots:
+            coordinates= evacuspot['geometry']['coordinates']
+            self.assertTrue(len(coordinates) == 2,'Missing field {}'.format(evacuspot))
+             
+            
     def test_unique_spot_number(self):
         ids = dict()
         for evacuspot in self.evacuspots:
-            spot_number = evacuspot['spot_number']
+            properties = evacuspot['properties']
+            spot_number = properties['spot_number']
             self.assertFalse(spot_number in ids, "Spot {} is not unique".format(spot_number))
             ids[spot_number] = True
+        self.assertEqual(len(ids), len(self.evacuspots))
 
     def test_geojson_required_fields(self):
         with open('evacuspots.geojson', 'r') as f:
